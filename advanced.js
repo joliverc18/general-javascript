e5f90d458481181e124a5da60ca2df757f7140f2
Joshua Oliver <41431789+joliverc18@users.noreply.github.com>
2019-07-02 23:35:50 +0800

    Learned different ways of creating objects

--- advanced.js
+++ advanced.js
@@ -1,5 +1,6 @@
+/*eslint-env browser*/
 // Function constructor
-
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
@@ -10,11 +11,13 @@ var Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
+    */
+
 //    this.calculateAge = function() {              // Uncomment this and comment prototype to see the difference with adding in constructor vs prototype
 //        console.log(2016 - this.yearOfBirth);
 //    };
-}
-
+//};
+// ----------------------------------------------------------------------
 // Adding a method that user wants to it to be inherited is better
 // than adding it directly in the constructor is because
 // adding it in constructor uses memory to store it
@@ -22,9 +25,10 @@ var Person = function(name, yearOfBirth, job) {
 // prototype does not include as a property of the instance
 // rather, the instance can access the method from the prototype
 // This approach saves time and is more efficient
+/*
 Person.prototype.calculateAge = function () {   // Method to be inherited
         console.log(2016 - this.yearOfBirth);
-    }
+    };
 
 Person.prototype.lastName = 'Smith';            // Property to be inherited
 
@@ -39,7 +43,8 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
-
+*/
+// ----------------------------------------------------------------------
 // To see the difference in adding in constructor/prototype
 // Uncomment/Comment the corresponding method in either constructor/prototype
 // And console.log each of it
@@ -51,8 +56,29 @@ console.log(mark.lastName);
 // objects and the prototype chain
 
 
+// .hasOwnProperty('job') to check if an object has a certain property
+// instanceof Person check if an object is an instance of another Object
+// ----------------------------------------------------------------------
 
+// Object.create
 
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, 
+{
+    name: {value: 'Jane'},
+    yearOfBirth: {value: 1969},
+    job: {value: 'designer'}
+});
 
 
 
