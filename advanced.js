fc588ee13bc522842dee79b8dd64cf7e99279052
Joshua Oliver <41431789+joliverc18@users.noreply.github.com>
2019-07-06 23:27:44 +0800

    Added IIFE

--- advanced.js
+++ advanced.js
@@ -175,6 +175,55 @@ console.log(obj.city);
 //var heartRates = arrayCalc(ages, maxHeartRate);
 //console.log(heartRates);
 
+// First class functions: returning functions
+
+//function interviewQuestion(job) {
+//    if (job === 'designer') {
+//        return function(name) {
+//            console.log(name + ', can you please explain what UX design is ?');
+//        };
+//    } else if (job === 'teacher') {
+//        return function (name) {
+//            console.log('What subject do you taech, ' + name + '?');  
+//        };
+//    } else {
+//        return function (name) {
+//            console.log('Hello ' + name + ', what do you do?');
+//        };
+//    }
+//}
+//
+//var teacherQuestion = interviewQuestion('teacher');
+//teacherQuestion('John');
+//
+//var designerQuestion = interviewQuestion('designer');
+//designerQuestion('John');
+
+// Function Immediately invoked function expression
+
+//function game() {
+//    var score = Math.random() * 10;
+//    console.log(score >= 5);
+//}
+//game();
+
+//(function () {
+//    var score = Math.random() * 10;
+//    console.log(score >= 5);
+//})();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
