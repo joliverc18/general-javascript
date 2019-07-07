22cb0dc942d0cb1ff679e049dca35a2bc6e25582
Joshua Oliver <41431789+joliverc18@users.noreply.github.com>
2019-07-07 23:33:37 +0800

    Added closures JS

--- advanced.js
+++ advanced.js
@@ -212,15 +212,58 @@ console.log(obj.city);
 //    console.log(score >= 5);
 //})();
 
+// Closures
+
+//function retirement(retirementAge) {
+//    var a = ' years left until retirement';
+//    return function (yearOfirth) {
+//        var age = 2016 - yearOfirth;
+//        console.log((retirementAge - age) + a);
+//    }
+//}
+//
+//
+//var retirementUS = retirement(66);
+//
+//retirementUS(1990);
+//retirement(66)(1990);  
+//
+//var retirementGermany = retirement(65);
+//var retirementIceland = retirement(67);
+//
+//retirementGermany(1990);
+//retirementIceland(1990);
+
+//function interviewQuestion(job) {
+//    if (job === 'designer') {
+//        return function(name) {
+//            console.log(name + ', can you please explain what UX design is ?');
+//        };
+//    } else if (job === 'teacher') {
+//        return function (name) {
+//            console.log('What subject do you taech, ' + name + '?');  
+//        };
+//    } else {
+//        return function (name) {
+//            console.log('Hello ' + name + ', what do you do?');
+//        };
+//    }
+//}
+
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is ?');
+        } else if (job === 'teacher') {
+                console.log('What subject do you taech, ' + name + '?');  
+        } else {
+                console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
 
 
-
-
-
-
-
-
-
+interviewQuestion('John')('designer');
 
 
 
